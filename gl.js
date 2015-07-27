1a8fd788c22aa33f61cb2d92253f22706c88db05
unknown <rj150@hotmail.com>
2015-07-27 14:25:16 -0400

    added glScissor and the deprecated functions glColor4f, glRect

--- gl.js
+++ gl.js
@@ -16,18 +16,18 @@ function trace(message) {
  */
 function main_GL() {
     "use strict";
-    var viewport_state = {v: []};
     var error_code;
 
     glClearColor(0, 1, 1, 0.5);
     glColorMask(GL_FALSE, GL_TRUE, GL_FALSE, GL_TRUE);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glColorMask(GL_TRUE, GL_FALSE, GL_TRUE, GL_TRUE);
+    glColor4f(1, 0, 0, 1);
+    glRect(-0.5, -0.5, +0.5, +0.5);
+
     glFlush();
     glFinish();
-
-    glGetIntegerv(GL.VIEWPORT, viewport_state);
-    glViewport(0, 0, viewport_state[2], viewport_state[3]);
 
     trace(glGetString(GL_VENDOR));
     trace(glGetString(GL_RENDERER));
@@ -215,6 +215,13 @@ function glColorMask(R, G, B, A) {
     return;
 } /* All versions of OpenGL and OpenGL ES have this function; it's universal. */
 
+function glScissor(x, y, width, height) {
+    "use strict";
+
+    GL.scissor(x, y, width, height);
+    return;
+} /* All versions of OpenGL and OpenGL ES have this function; it's universal. */
+
 function glFlush() {
     "use strict";
 
@@ -295,4 +302,89 @@ function glGetString(name) {
         return (ext_list);
     }
     return GL.getParameter(name);
+}
+
+/*
+ * OpenGL 1.x functions removed by Khronos in OpenGL ES 2+
+ *
+ * I don't care to support all of them, just some of them.
+ *
+ * For example, it's impossible to draw a single point, pixel or vertex in
+ * OpenGL in a COMPATIBLE way for older systems and the "deprecation" model
+ * at the same time, which effectively makes OpenGL a dead language since
+ * there is no other alternative to OpenGL which is nearly as cross-platform.
+ *
+ * Thank Khronos.
+ */
+
+var ortho_x1 = -1,
+    ortho_x2 = +1,
+    ortho_y1 = -1,
+    ortho_y2 = +1,
+    ortho_z1 = -1,
+    ortho_z2 = +1;
+
+var color_red = 1,
+    color_green = 1,
+    color_blue = 1,
+    color_coverage = 1;
+
+function glColor4f(red, green, blue, alpha) {
+    "use strict";
+
+/*
+ * glColor* is all removed from OpenGL ES 2+.
+ * However, glColor4f, in particular, was available on OpenGL ES 1.0.
+ */
+    color_red = red;
+    color_green = green;
+    color_blue = blue;
+
+    color_coverage = alpha;
+    return;
+}
+
+function glRect(x1, y1, x2, y2) {
+    "use strict";
+    var viewport_state = [];
+    var scissoring_enabled;
+    var raster_x1, raster_y1, raster_x2, raster_y2, delta_x, delta_y;
+    var temporary;
+
+/*
+ * glRectf, *fv, *d, *dv are deprecated but pretty easy to emulate by using
+ * glClear with scissoring to create the rectangle using raster operations.
+ */
+    scissoring_enabled = glIsEnabled(GL_SCISSOR_TEST);
+    if (scissoring_enabled === GL_FALSE) {
+        glEnable(GL_SCISSOR_TEST);
+    }
+
+    viewport_state = GL.getParameter(GL.VIEWPORT);
+    raster_x1 = (x1 - ortho_x1)/2 * (viewport_state[2] - 1);
+    raster_y1 = (y1 - ortho_y1)/2 * (viewport_state[2] - 1);
+    raster_x2 = (2*ortho_x2 - x2)/2 * (viewport_state[3] - 1);
+    raster_y2 = (2*ortho_y2 - y2)/2 * (viewport_state[3] - 1);
+
+    if (raster_x2 < raster_x1) {
+        temporary = raster_x1;
+        raster_x1 = raster_x2;
+        raster_x2 = temporary;
+    }
+    if (raster_y2 < raster_y1) {
+        temporary = raster_y1;
+        raster_y1 = raster_y2;
+        raster_y2 = temporary;
+    }
+    delta_x = raster_x2 - raster_x1;
+    delta_y = raster_y2 - raster_y1;
+    glScissor(raster_x1, raster_y1, delta_x, delta_y);
+
+    glClearColor(color_red, color_green, color_blue, color_coverage);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    if (scissoring_enabled === GL_FALSE) {
+        glDisable(GL_SCISSOR_TEST);
+    }
+    return;
 }
