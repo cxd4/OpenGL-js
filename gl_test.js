12e7850a902b7b1b25b0977af3dac59eee906b40
no <rj150@hotmail.com>
2015-08-09 20:35:56 -0400

    Halve the memory needed to draw the circle.

--- gl_test.js
+++ gl_test.js
@@ -38,7 +38,7 @@ function display() {
  * Draw the unit circle (a circle with a radius of 1.0) to circumscribe
  * the perfect triangle, which will be drawn in front of it.
  */
-    glVertexPointer(2, GL_FLOAT, 4 * 4, circle);
+    glVertexPointer(2, GL_FLOAT, 0, circle);
     glDrawArrays(GL_TRIANGLE_FAN, 0, 360 + 1 + 1);
 
     glEnableClientState(GL_VERTEX_ARRAY);
@@ -59,13 +59,13 @@ function init() {
     "use strict";
     var i;
 
-    circle[4 * 0 + 0] = circle[4 * 0 + 1] = 0.0;
+    circle[2 * 0 + 0] = circle[2 * 0 + 1] = 0.0;
     for (i = 0 + 1; i < 360 + 1; i += 1) {
-        circle[4 * i + 0] = Math.cos(i * Math.PI / 180);
-        circle[4 * i + 1] = Math.sin(i * Math.PI / 180);
+        circle[2 * i + 0] = Math.cos(i * Math.PI / 180);
+        circle[2 * i + 1] = Math.sin(i * Math.PI / 180);
     }
-    circle[4 * i + 0] = circle[4 * 1 + 0];
-    circle[4 * i + 1] = circle[4 * 1 + 1];
+    circle[2 * i + 0] = circle[2 * 1 + 0];
+    circle[2 * i + 1] = circle[2 * 1 + 1];
 
     glEnable(GL_BLEND);
     glDisable(GL_CULL_FACE);
