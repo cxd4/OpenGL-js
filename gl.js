d5b7ab8e15bf693b8567c5f748ad77cef940ec89
Iconoclast <cxd4@uniplex.cat-vs-internet.gov>
2018-11-28 19:40:37 -0500

    How about this?

--- gl.js
+++ gl.js
@@ -1,27 +1,26 @@
-/**
- * C-style OpenGL (ES) wrapper defined using JavaScript WebGL.
- * @source: http://cxd4.github.io/OpenGL-js/
- *
- * @licstart
- * Copyright (C) 2015  rjs
- *
- * The JavaScript code in this page is free software: you can
- * redistribute it and/or modify it under the terms of the GNU
- * General Public License (GNU GPL) as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.  The code is distributed WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
- *
- * As additional permission under GNU GPL version 3 section 7, you
- * may distribute non-source (e.g., minimized or compacted) forms of
- * that code without the copy of the GNU GPL normally required by
- * section 4, provided you include this license notice and a URL
- * through which recipients can access the Corresponding Source.
- * @licend
- */
+// @source: https://cxd4.github.io/OpenGL-js/
+/* @licstart  The following is the entire license notice for the 
+  JavaScript code in this page.
 
+  Copyright (C) 2015 me
 
+  The JavaScript code in this page is free software: you can
+  redistribute it and/or modify it under the terms of the GNU
+  General Public License (GNU GPL) as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option)
+  any later version.  The code is distributed WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+
+  As additional permission under GNU GPL version 3 section 7, you
+  may distribute non-source (e.g., minimized or compacted) forms of
+  that code without the copy of the GNU GPL normally required by
+  section 4, provided you include this license notice and a URL
+  through which recipients can access the Corresponding Source.
+
+  @licend  The above is the entire license notice
+  for the JavaScript code in this page.
+*/
 var GL; /* global context name for setting up C emulation in JavaScript */
 var GL_state = {};
 
