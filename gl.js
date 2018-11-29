14b733b4e8a6ec7876dcf25f813250d908e6130e
Iconoclast <cxd4@uniplex.cat-vs-internet.gov>
2018-11-29 00:55:30 -0500

    Is the copyright needed to pass the validation?

--- gl.js
+++ gl.js
@@ -3,10 +3,6 @@
 The following is the entire license notice for the JavaScript code in this file.
 */
 /*
- *
- * Copyright (C) 2015 me
- *
- *
  * The JavaScript code in this page is free software: you can
  * redistribute it and/or modify it under the terms of the GNU
  * General Public License (GNU GPL) as published by the Free Software
@@ -14,12 +10,6 @@ The following is the entire license notice for the JavaScript code in this file.
  * any later version.  The code is distributed WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
- *
- * As additional permission under GNU GPL version 3 section 7, you
- * may distribute non-source (e.g., minimized or compacted) forms of
- * that code without the copy of the GNU GPL normally required by
- * section 4, provided you include this license notice and a URL
- * through which recipients can access the Corresponding Source.
  */
 /*
 @licend
