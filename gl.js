243a3e735c84650474c7eaea0865cb255fb78908
Iconoclast <cxd4@uniplex.cat-vs-internet.gov>
2018-11-28 22:56:23 -0500

    whitespace

--- gl.js
+++ gl.js
@@ -1,5 +1,5 @@
-/* @licstart The following is the entire license notice for the
-  JavaScript code in this page.
+/*
+@licstart The following is the entire license notice for the JavaScript code in this page.
 
   Copyright (C) 2015 me
 
@@ -17,8 +17,7 @@
   section 4, provided you include this license notice and a URL
   through which recipients can access the Corresponding Source.
 
-  @licend The above is the entire license notice
-  for the JavaScript code in this page.
+@licend The above is the entire license notice for the JavaScript code in this page.
 */
 
 var GL; /* global context name for setting up C emulation in JavaScript */
