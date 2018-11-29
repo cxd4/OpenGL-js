8c220ddf7ef0d7ba7289ffa728d2a2b5bceb64a3
Iconoclast <cxd4@uniplex.cat-vs-internet.gov>
2018-11-29 14:55:26 -0500

    Correct the license expression back to CC0, as we've been using that.

--- gl.js
+++ gl.js
@@ -1,4 +1,6 @@
-// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
+/*
+@license magnet:?xt=urn:btih:90dc5c0be029de84e523b9b3922520e79e0e6f08&dn=cc0.txt CC0-1.0
+*/
 
 var GL; /* global context name for setting up C emulation in JavaScript */
 var GL_state = {};
